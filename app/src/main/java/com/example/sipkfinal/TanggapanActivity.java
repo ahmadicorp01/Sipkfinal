beedbd1c0bc2b966377fb62845102ae00a02f5be
cupiz <spfindo@gmail.com>
2019-03-05 23:24:38 +0700

    Chatting selesai

--- app/src/main/java/com/example/sipkfinal/TanggapanActivity.java
+++ app/src/main/java/com/example/sipkfinal/TanggapanActivity.java
@@ -8,12 +8,25 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 public class TanggapanActivity extends AppCompatActivity {
@@ -21,6 +34,8 @@ public class TanggapanActivity extends AppCompatActivity {
     private ChatAdapter cAdapter;
     private RecyclerView.LayoutManager cLayoutManager;
 
+    EditText tanggapan;
+    Button kirim;
     int id_user = 0;
     SharedPreferences sharedPreferences;
     private ArrayList<ChatItem> chatItems;
@@ -33,6 +48,21 @@ public class TanggapanActivity extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("SIPK", MODE_PRIVATE);
         id_user = sharedPreferences.getInt("id_user", 0);
 
+        Intent intent = getIntent();
+        final String id_laporan = intent.getExtras().getString("id_laporan");
+
+        tanggapan = findViewById(R.id.text_pesan);
+        kirim = findViewById(R.id.btn_kirim);
+        kirim.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new sendChat().execute(Integer.toString(id_user),
+                        id_laporan,
+                        tanggapan.getText().toString());
+            }
+        });
+
         chatItems = new ArrayList<>();
         cRecyclerView = findViewById(R.id.recycler_chat);
 
@@ -42,8 +72,6 @@ public class TanggapanActivity extends AppCompatActivity {
         cRecyclerView.setLayoutManager(cLayoutManager);
         cRecyclerView.setAdapter(cAdapter);
 
-        Intent intent = getIntent();
-        String id_laporan = intent.getExtras().getString("id_laporan");
 
         new UserChatDaftar().execute(id_laporan);
 
@@ -82,6 +110,71 @@ public class TanggapanActivity extends AppCompatActivity {
             String jsonStr = sh.makeServiceCall(url);
 
             return jsonStr;
+        }
+    }
+
+    private class sendChat extends AsyncTask <String,Void,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String reg_url = "http://10.0.2.2/sipk/api/user_tanggapan_kirim/";
+            String text = "";
+
+            Log.d("SIPK", reg_url);
+
+            String id_pengguna = params[0];
+            String id_laporan = params[1];
+            String tanggapan = params[2];
+            String dibaca = "1";
+
+            try{
+                URL url = new URL(reg_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+
+                        String data = URLEncoder.encode("id_pengguna","UTF-8") + "=" + URLEncoder.encode(id_pengguna, "UTF-8") + "&" +
+                                URLEncoder.encode("id_laporan","UTF-8") + "=" + URLEncoder.encode(id_laporan, "UTF-8") + "&" +
+                        URLEncoder.encode("tanggapan", "UTF-8") + "=" +  URLEncoder.encode(tanggapan,"UTF-8") + "&" +
+                        URLEncoder.encode("dibaca", "UTF-8") + "=" +  URLEncoder.encode(dibaca,"UTF-8");
+
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+
+                int statusCode = httpURLConnection.getResponseCode();
+                Log.d("SIPK", "Status: " + statusCode);
+
+                if(statusCode == 200){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while (((line = reader.readLine()))!=null)
+                        sb.append(line).append("\n");
+                    text = sb.toString();
+                    bufferedWriter.close();
+                }
+            }
+            catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("SIPK", result);
+
+//            try{
+//                JSONObject obj = new JSONObject(result);
+//                Toast.makeText(LaporActivity.this, obj.getString("data"), Toast.LENGTH_SHORT).show();
+//            }catch (JSONException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
